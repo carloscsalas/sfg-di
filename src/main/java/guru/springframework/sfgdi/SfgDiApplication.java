8f375b01eea125f4e8a4f9a1cb8009f5c4e574b2
gino <jaime.fernandez.miranda.contractor@bbva.com>
2021-10-16 16:44:46 -0500

    get spring context and invoke a bean(myController)

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		/*run, this is actually going to turn back a application context*/
+//		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		//I can actually ask that context for an instance of my controller
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+
 	}
 
 }
